f216c65f4ce9c39c0fc08c8739939de0a966ec0d
Евгений Астахов <evastahov@MacBookAir.netis.cc>
2025-04-22 18:41:37 -0700

    step 1

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(["node_modules/*", "dist/*"]),
+  globalIgnores(['node_modules/*', 'dist/*']),
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.browser },
   },
 ]);
